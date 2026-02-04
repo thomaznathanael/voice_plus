d73dcaed1735838098c12db9431ece62a9130628
Thomaz Nathanael <thomaznathanael@gmail.com>
2026-02-04 01:49:57 -0300

    0.2

--- client.lua
+++ client.lua
@@ -242,7 +242,7 @@ addEventHandler("voice_local:playRadioRogerNearby", root, function(sourcePlayer)
     local localX, localY, localZ = getElementPosition(localPlayer)
     local sourceX, sourceY, sourceZ = getElementPosition(sourcePlayer)
     local distance = getDistanceBetweenPoints3D(localX, localY, localZ, sourceX, sourceY, sourceZ)
-    local volume = computeDistanceVolume(distance, maxDistance) * 0.5
+    local volume = computeDistanceVolume(distance, maxDistance) * 0.2
     if volume <= 0.0 then
         return
     end
